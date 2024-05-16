554b9083a9759fe6a6eb50f9dd93cbed721f8ee4
fatima856 <155239967+fatima856@users.noreply.github.com>
2024-05-16 22:29:51 +0300

    Add files via upload

---
+++ client.py
@@ -0,0 +1,9 @@
+import socket
+
+HOST = "127.0.0.1"
+PORT = 65432
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect((HOST, PORT))
+s.send(b"Hello , World.... sent by client")
+data = s.recv(1024)
+print(f"Received {data!r}")
